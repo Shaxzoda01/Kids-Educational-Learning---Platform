24692a6b447cd68a56403d33cfdbbe5d534d3d91
shaxzoda01 <shaxzoda01062005@gmail.com>
2025-11-25 16:12:31 +0500

    finished auth page

---
+++ app.js
@@ -0,0 +1,16 @@
+function saveData(e) {
+    e.preventDefault();
+
+    const nameValue = document.getElementById('parentName').value.trim();
+    const emailValue = document.getElementById('email').value.trim();
+    const passwordValue = document.getElementById('password').value.trim();
+
+    localStorage.setItem('parentName', nameValue);
+    localStorage.setItem('email', emailValue);
+    localStorage.setItem('password', passwordValue);
+
+    console.log('reg info added to localstorage');
+
+    window.location.href = 'main.html';
+}
+
